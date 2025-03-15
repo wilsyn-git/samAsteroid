5c10ba7b436bfa398005208861db8d3f28e992a1
wilsyn-git <orders@siraim.com>
2025-03-14 23:11:22 -0400

    changed clock stuff

--- main.py
+++ main.py
@@ -7,9 +7,13 @@ from constants import *
 
 
 def main():
+
+    dt = 0
+
     print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+
+    clock = pygame.time.Clock()
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     BLACK = (0,0,0)
@@ -20,6 +24,9 @@ def main():
             if event.type == pygame.QUIT:
                 return
         pygame.display.flip()
+        dt = (clock.tick(60))/1000
+
+
 
 if __name__ == "__main__":
     main()
