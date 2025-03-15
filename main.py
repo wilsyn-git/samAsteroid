282f561941af5bd10af7226098500086e9bee93a
wilsyn-git <orders@siraim.com>
2025-03-14 23:06:30 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+
+import pygame
+from constants import *
+
+
+def main():
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    BLACK = (0,0,0)
+    screen.fill(BLACK)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        pygame.display.flip()
+
+if __name__ == "__main__":
+    main()
